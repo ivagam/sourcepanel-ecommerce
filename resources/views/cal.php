248bb3d4a2f7e9fba37352640a6ab7141734990d
saravananm1996 <160225112+saravananm1996@users.noreply.github.com>
2026-02-07 09:59:27 +0530

    cal update

--- resources/views/cal.php
+++ resources/views/cal.php
@@ -191,7 +191,7 @@
             if (divided <= 65) price = divided + 40;
             else if (divided <= 150) price = divided * 1.6;
             else if (divided <= 199) price = divided * 1.595;
-            else price = divided * 1.5;
+            else price = divided * 1.6;
         } else if (category === '1') {
             if (divided <= 100) price = divided + 80;
             else if (divided <= 199) price = divided + 90;
